9ff3991f3170d79e04ac9c31870b79ee99ee0b29
SolanaExperiments <alexpr89@gmail.com>
2025-06-11 13:03:51 -0400

    HW7

--- homeworks_rust/homeworks/homework7/modules/modules2.rs
+++ homeworks_rust/homeworks/homework7/modules/modules2.rs
@@ -3,13 +3,11 @@
 // 'use' and 'as' keywords. Fix these 'use' statements to make the code compile.
 // Make me compile! Execute `rustlings hint modules2` for hints :)
 
-// I AM NOT DONE
-
 mod delicious_snacks {
 
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // Fix: these are fine if used inside this module
+    use self::fruits::PEAR as fruit;
+    use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
@@ -20,12 +18,12 @@ mod delicious_snacks {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
-}
 
+    pub fn print_snacks() {
+        println!("Fruit: {}", fruit);
+        println!("Veggie: {}", veggie);
+    }
+}
 fn main() {
-    println!(
-        "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
-    );
+    delicious_snacks::print_snacks();
 }
