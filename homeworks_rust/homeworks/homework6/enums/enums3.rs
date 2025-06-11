0b73348529519a1d177493268e5eb842c3392470
SolanaExperiments <alexpr89@gmail.com>
2025-06-11 12:28:28 -0400

    HW6

--- homeworks_rust/homeworks/homework6/enums/enums3.rs
+++ homeworks_rust/homeworks/homework6/enums/enums3.rs
@@ -1,10 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)), // RGB color tuple
+    Echo(String),              // String message
+    Move(Point),               // Point struct for position
+    Quit,                      // Quit message
 }
 
 struct Point {
@@ -36,6 +37,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+        match message {
+            Message::ChangeColor(color) => self.change_color(color),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(p) => self.move_position(p),
+            Message::Quit => self.quit(),
+        }
         // TODO: create a match expression to process the different message variants
     }
 }
